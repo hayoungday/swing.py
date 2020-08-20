7e6e81238532f3d6e3ea05e9f989efd4c9432746
hayoungday <yha3065@naver.com>
2020-08-20 18:50:56 +0900

    swingpage - Post

--- swingpage/views.py
+++ swingpage/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView
+
 
 # Create your views here.
+
+# class PostList(ListView):
+#     model = Post
+
+def index(request):
+    posts = Post.objects.all()
+
+    return render(
+        request,
+        'swingpage/index.html',
+        {
+            'posts' : posts,
+            'a_plus_b' : 1+3
+        }
+    )
